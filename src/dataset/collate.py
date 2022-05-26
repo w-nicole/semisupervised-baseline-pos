3d102d364de1f25da827d0b621821a4a790680da
Nicole Wong <wongn@mit.edu>
2022-05-26 18:27:45 -0400

    Revert "retry merge"
    This reverts commit 2f2e49aa1bc4882a059062b59f653155c0011445.

--- src/dataset/collate.py
+++ src/dataset/collate.py
@@ -11,8 +11,6 @@ def average_embeddings(padded_embeddings, padded_start_indices, padded_end_indic
     Expects padded_embeddings to NOT have CLS/SEP removed.
     """
 
-    padding_value = 0
-
     # Below is valid because real averaging indices are given from 1
     assert constant.START_END_INDEX_PADDING < 1
     clean_indices = lambda indices : indices[indices != constant.START_END_INDEX_PADDING]
@@ -36,17 +34,13 @@ def average_embeddings(padded_embeddings, padded_start_indices, padded_end_indic
             clean_averaged_embeddings.append(average)
         
         assert start_indices.shape[0] == len(clean_averaged_embeddings)
-       
-        # Handle empty sentence examples
-        to_use_embeddings = clean_averaged_embeddings if clean_averaged_embeddings else [torch.zeros((padded_embeddings.shape[-1],))]
-        sentence_embeddings = torch.stack(to_use_embeddings, axis = 0)
+        sentence_embeddings = torch.stack(clean_averaged_embeddings, axis = 0)
 
         all_clean_averaged_embeddings.append(sentence_embeddings)
     
     padded_averages = torch.nn.utils.rnn.pad_sequence(
         all_clean_averaged_embeddings,
-        batch_first = True,
-        padding_value = padding_value
+        batch_first = True
     )
     
     assert padded_averages.shape[0] == padded_embeddings.shape[0]
--- src/model/base.py
+++ src/model/base.py
@@ -5,6 +5,8 @@
 # Changes made relative to original:
 # Changed hyperparameters to match those in the paper,
 # Added averaging behavior.
+# Changed to not support weighted features, but instead a concatenation of all hidden representations.
+# Changed to support single hidden layer MLP.
 
 import hashlib
 import json
@@ -69,7 +71,8 @@ class Model(pl.LightningModule):
             self.mapping = torch.load(hparams.mapping)
             util.freeze(self.mapping)
 
-        self.projector = self.build_projector()
+        # Changed below line
+        self.projector = None
         self.dropout = InputVariationalDropout(hparams.input_dropout)
 
     def build_model(self):
@@ -119,17 +122,6 @@ class Model(pl.LightningModule):
 
     @property
     def hidden_size(self):
-        if isinstance(self.model, transformers.BertModel) or isinstance(
-            self.model, transformers.RobertaModel
-        ):
-            return self.model.config.hidden_size
-        elif isinstance(self.model, transformers.XLMModel):
-            return self.model.dim
-        else:
-            raise ValueError("Unsupported model")
-
-    @property
-    def num_layers(self):
         # hidden_size = the input to the classifier
         if isinstance(self.model, transformers.BertModel) or isinstance(
             self.model, transformers.RobertaModel
@@ -178,23 +170,6 @@ class Model(pl.LightningModule):
     def reset_metrics(self):
         for metric in self.metrics.values():
             metric.reset()
-
-    def build_projector(self):
-        hparams = self.hparams
-        if hparams.projector == "id":
-            return Identity()
-        elif hparams.projector == "meanpool":
-            return MeanPooling()
-        elif hparams.projector == "transformer":
-            return Transformer(
-                input_dim=self.hidden_size,
-                hidden_dim=hparams.projector_trm_hidden_size,
-                num_heads=hparams.projector_trm_num_heads,
-                dropout=hparams.projector_dropout,
-                num_layers=hparams.projector_trm_num_layers,
-            )
-        else:
-            raise ValueError(hparams.projector)
 
     def get_mask(self, sent: Tensor):
         mask = (sent != self.tokenizer.pad_token_id).long()
@@ -248,7 +223,7 @@ class Model(pl.LightningModule):
         hs = self.map_feature(output["hidden_states"], langs)
         hs = self.process_feature(hs)
         hs = self.dropout(hs)
-        hs = self.projector(hs, mask)
+        # Removed projector.
         
         # Below: added averaging.
         averaged_hs = collate.average_embeddings(hs, start_indices, end_indices)
@@ -434,7 +409,6 @@ class Model(pl.LightningModule):
         **kwargs,
     ):
         datasets = []
-
         for lang in langs:
             filepath = data_class.get_file(self.hparams.data_dir, lang, split)
             if filepath is None:
@@ -549,7 +523,12 @@ class Model(pl.LightningModule):
         parser.add_argument("--pretrain", required=True, type=str)
         parser.add_argument("--freeze_layer", default=-1, type=int)
         parser.add_argument("--feature_layer", default=-1, type=int)
-        parser.add_argument("--weighted_feature", default=False, type=util.str2bool)
+        # Below additions
+        parser.add_argument("--use_hidden_layer", default=False, type=util.str2bool)
+        parser.add_argument("--hidden_layer_size", default=-1, type=int)
+        # end additions
+        # Below line: changed from providing weighted features to a concatenated all hidden states
+        parser.add_argument("--concat_all_hidden_states", default=False, type=util.str2bool)
         parser.add_argument("--projector", default="id", choices=["id", "meanpool", "transformer"], type=str)
         parser.add_argument("--projector_trm_hidden_size", default=3072, type=int)
         parser.add_argument("--projector_trm_num_heads", default=12, type=int)
