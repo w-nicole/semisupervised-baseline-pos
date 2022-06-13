fixed kl reconstruction

src/explore_kl_reconstruction.py
@@ -32,31 +32,26 @@ def reconstruct_kl_over_checkpoints():
         current_predictions
     
 def calculate_kl_tensor_against_prior(log_q_given_input, prior):
-    
-    repeated_prior = prior.unsqueeze(0).repeat(log_q_given_input.shape[0], 1)
+    repeated_prior = util.apply_gpu(prior.unsqueeze(0).repeat(log_q_given_input.shape[0], 1))
     pre_sum = torch.exp(log_q_given_input) * (log_q_given_input - torch.log(repeated_prior))
     assert pre_sum.shape == log_q_given_input.shape, f'pre_sum: {pre_sum.shape}, q_given_input: {log_q_given_input.shape}'
-    
     return pre_sum
-
-def clean_1d_padding(padded_labels, tensor):
-    repeated_labels = padded_labels.unsqueeze(1).repeat(1, tensor.shape[-1])
-    mask = repeated_labels != LABEL_PAD_ID
-    clean_tensor = torch.where(mask, tensor, torch.zeros(tensor.shape))
-    return clean_tensor
-
-def reconstruct_kl_tensor(model, lang, predictions_dict, analysis_path):
-    raw_flat_softmaxes = torch.log(predictions_dict[lang])
-    flat_padded_labels = explore_predict.get_padded_labels(model, lang)
     
-    # Do equivalent of set_padded_to_zero using the current dimensionalities
-    clean_log_pi_t = clean_1d_padding(flat_padded_labels, raw_flat_softmaxes)
+def reconstruct_kl_tensor(model, lang, predictions_dict, analysis_path):
+    log_q_given_input = torch.cat([
+        outputs[1].sum(dim=1)
+        for outputs in predictions_dict[lang]
+    ], axis = 0)
+    
+    print(log_q_given_input.shape)
     
     model_prior = model.prior_param.prior
     english_prior = model.get_smoothed_english_prior()
     
-    kl_tensor_model = calculate_kl_tensor_against_prior(clean_log_pi_t, model_prior)
-    kl_tensor_english = calculate_kl_tensor_against_prior(clean_log_pi_t, english_prior)
+    kl_tensor_model = calculate_kl_tensor_against_prior(log_q_given_input, model_prior)
+    kl_tensor_english = calculate_kl_tensor_against_prior(log_q_given_input, english_prior)
+    
+    print('kl tensor model', kl_tensor_model.shape)
     
     tensor_path = os.path.join(analysis_path, f'{lang}_kl_tensors.pt')
     results = {
@@ -66,7 +61,6 @@ def reconstruct_kl_tensor(model, lang, predictions_dict, analysis_path):
         'english_prior' : english_prior
     }
     torch.save(results, tensor_path)
-    import pdb; pdb.set_trace()
     print(f'Written KL tensor reconstruction to {tensor_path}')
     return results
     
@@ -82,7 +76,7 @@ def save_histogram_kl_examples(kl_tensor_results, lang, analysis_path):
             label = prior_name, alpha = 0.5, color = color
         )
     
-    plt.title(f'KL of {lang} examples')
+    plt.title(f'KL of {lang} examples. Mean: {kl_divergence.mean()}')
     plt.xlabel('KL')
     plt.ylabel("Frequency")
     plt.legend()
@@ -109,7 +103,8 @@ if __name__ == '__main__':
         predictions_dict = explore_predict.get_all_predictions(model, [lang])
         
     kl_path = os.path.join(analysis_path, f'{lang}_kl_tensors.pt')
-    if False:#os.path.exists(kl_path):
+    
+    if os.path.exists(kl_path):
         all_kl = torch.load(kl_path)
     else:
         all_kl = reconstruct_kl_tensor(model, lang, predictions_dict, analysis_path)

src/explore_predict.py
@@ -18,6 +18,7 @@ import metric
 from pprint import pprint
 
 def get_all_predictions(model, langs):
+    predictions = {}
     for lang in langs:
         if lang == "English" and isinstance(model, VAE):
             print('Skipping English because it is a labeled case.')
@@ -25,25 +26,8 @@ def get_all_predictions(model, langs):
         trainer = pl.Trainer(gpus = 1 if torch.cuda.is_available() else 0)
         model.reset_metrics()
         dataloader = model.get_dataloader(lang, Split.dev)
-        current_predictions = trainer.predict(model, dataloaders = [dataloader], return_predictions = True)
-    return current_predictions
-    
-    
-def predict_validation(model, langs):
-    current_predictions = get_all_predictions(model, langs)
-    predictions = {}
-    try:
-        predictions[lang] = util.remove_from_gpu(
-            torch.cat([
-                        F.softmax(outputs[1].reshape(-1, model.nb_labels), dim = -1)
-                        for outputs in current_predictions
-                    ], dim = 0
-            )
-        )
-    except:
-        import pdb; pdb.set_trace()
+        predictions[lang] = trainer.predict(model, dataloaders = [dataloader], return_predictions = True)
     return predictions
-    
     
 def get_analysis_path(checkpoint_path):
     path_components = checkpoint_path.split('/')
@@ -52,18 +36,6 @@ def get_analysis_path(checkpoint_path):
     analysis_path = os.path.join(decoder_folder, os.path.splitext(checkpoint_name)[0])
     print(f'Analysis path: {analysis_path}')
     return analysis_path
-        
-        
-def get_validation_predictions(model, checkpoint_path):
-    
-    analysis_path = get_analysis_path(checkpoint_path)
-    if not os.path.exists(analysis_path):
-        os.makedirs(analysis_path)
-    
-    predictions = predict_validation(model, ['English', 'Dutch'])
-    torch.save(predictions, os.path.join(analysis_path, 'predictions.pt'))
-    
-    return predictions
     
 def get_padded_labels(model, lang):
     dataloader = model.get_dataloader(lang, Split.dev)
@@ -89,7 +61,7 @@ def compare_validation_predictions(model, checkpoint_path):
     
     try:
         for lang, raw_logits in predictions.items():
-            to_flat_label = lambda flat_probs : torch.argmax(flat_probs, dim = -1)
+            to_flat_label = lambda probs : torch.argmax(probs.reshape(-1, probs.shape[-1]), dim = -1)
             raw_outputs = torch.argmax(raw_logits, dim = -1)
             outputs, labels = clean_padded_labels_pair(model, lang, raw_outputs)
     
@@ -153,11 +125,15 @@ if __name__ == '__main__':
     
     checkpoint_path = os.path.join(model_folder, 'ckpts', checkpoint_name)
     model = model_type.load_from_checkpoint(checkpoint_path)
-    #get_validation_predictions(model, checkpoint_path)
+    predictions = get_all_predictions(model, ['Dutch'])
+    
+    analysis_path = get_analysis_path(checkpoint_path)
+    torch.save(predictions, os.path.join(analysis_path, 'predictions.pt'))
+    
     #compare_validation_predictions(model, checkpoint_path)
     #results = compare_english_prior(model)
     
-    pprint(results)
+    print(predictions)
     
     
     

src/model/base.py
@@ -274,9 +274,7 @@ class Model(pl.LightningModule):
         aver_result = defaultdict(list)
         for lang, output in zip(langs, outputs):
             for key in output[0]:
-                try:
-                    mean_val = torch.stack([x[key] for x in output]).mean()    
-                except: import pdb; pdb.set_trace()
+                mean_val = torch.stack([x[key] for x in output]).mean()
                 self.log(key, mean_val)
 
                 raw_key = key.replace(f"{lang}_", "")

src/model/vae.py
@@ -29,7 +29,7 @@ class VAE(BaseVAE):
         self.prior_param['prior'] = torch.nn.Parameter(prior, requires_grad = True)
         
         assert len(self.hparams.val_langs) == 1, "Validation prior code currently only designed for one validation language."
-        self.validation_prior = util.apply_gpu(self.get_prior(self.hparams.val_langs[0], Split.dev))
+        self.validation_prior = util.apply_gpu(self.get_smoothed_prior(self.hparams.val_langs[0], Split.dev))
         self._selection_criterion = 'val_acc'
         self._comparison_mode = 'max'
     
@@ -40,14 +40,17 @@ class VAE(BaseVAE):
     def get_prior(self, lang, split):
         counts = self.get_label_counts(lang, split)
         return counts / torch.sum(counts)
-        
-    def get_smoothed_english_prior(self):
+    
+    def get_smoothed_prior(self, lang, split):
         threshold = 0.001
-        prior = self.get_prior('English', Split.train)
+        prior = self.get_prior(lang, split)
         raw_smoothed_prior = torch.clamp(prior, min = threshold)
-
         assert torch.all(raw_smoothed_prior >= threshold), raw_smoothed_prior
         return raw_smoothed_prior / torch.sum(raw_smoothed_prior)
+        
+    def get_smoothed_english_prior(self):
+        prior = self.get_smoothed_prior('English', Split.train)
+        return prior
     
     def calculate_log_pi_t(self, batch, hs):
         logits = self.classifier(hs)
@@ -101,7 +104,6 @@ class VAE(BaseVAE):
             loss['target_KL'] = self.calculate_KL_against_prior(log_q_given_input, self.validation_prior)
             
         loss['decoder_loss'] = loss['MSE'] + self.hparams.kl_weight * loss['KL']
-        
         return loss, log_pi_t
         
     @classmethod