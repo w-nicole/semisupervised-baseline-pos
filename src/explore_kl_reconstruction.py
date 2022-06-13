changes to fix kl, but not propagated to explore scripts

src/explore_kl_reconstruction.py
@@ -38,12 +38,8 @@ def calculate_kl_tensor_against_prior(log_q_given_input, prior):
     return pre_sum
     
 def reconstruct_kl_tensor(model, lang, predictions_dict, analysis_path):
-    log_q_given_input = torch.cat([
-        outputs[1].sum(dim=1)
-        for outputs in predictions_dict[lang]
-    ], axis = 0)
     
-    print(log_q_given_input.shape)
+    log_q_given_input = explore_predict.get_log_q_given_input(predictions_dict, lang)
     
     model_prior = model.prior_param.prior
     english_prior = model.get_smoothed_english_prior()
@@ -85,7 +81,6 @@ def save_histogram_kl_examples(kl_tensor_results, lang, analysis_path):
     plt.savefig(figure_path)
     
     print(f'Written to {figure_path}')
-
     
 if __name__ == '__main__':
     
@@ -103,12 +98,13 @@ if __name__ == '__main__':
         predictions_dict = explore_predict.get_all_predictions(model, [lang])
         
     kl_path = os.path.join(analysis_path, f'{lang}_kl_tensors.pt')
+    # if os.path.exists(kl_path):
+    #     all_kl = torch.load(kl_path)
+    # else:
+    #     all_kl = reconstruct_kl_tensor(model, lang, predictions_dict, analysis_path)
+    # save_histogram_kl_examples(all_kl, lang, analysis_path)
     
-    if os.path.exists(kl_path):
-        all_kl = torch.load(kl_path)
-    else:
-        all_kl = reconstruct_kl_tensor(model, lang, predictions_dict, analysis_path)
-    save_histogram_kl_examples(all_kl, lang, analysis_path)
+    
     
 
 

src/explore_predict.py
@@ -45,49 +45,64 @@ def get_padded_labels(model, lang):
     labels = torch.cat([batch['labels'].flatten() for batch in dataloader])
     return labels
 
-def clean_padded_labels_pair(model, lang, padded_predictions):
+def clean_padded_labels_pair(model, lang, padded_predictions, number_of_dims):
     padded_labels = util.remove_from_gpu(get_padded_labels(model, lang))
     mask_for_non_pad = (padded_labels != metric.LABEL_PAD_ID)
     labels = padded_labels[mask_for_non_pad]
-    outputs = padded_predictions[mask_for_non_pad]
+    if number_of_dims == 1:
+        outputs = padded_predictions[mask_for_non_pad]
+    elif number_of_dims == 2:
+        outputs = padded_predictions[mask_for_non_pad, :]
+    elif number_of_dims == 3:
+        outputs = padded_predictions[mask_for_non_pad, :, :]
+    else:
+        assert False, "This number of dimensions is not supported."
     return outputs, labels
     
     
-def compare_validation_predictions(model, checkpoint_path):
+# def compare_validation_predictions(model, checkpoint_path):
     
-    analysis_path = get_analysis_path(checkpoint_path)
+#     analysis_path = get_analysis_path(checkpoint_path)
+#     predictions = torch.load(os.path.join(analysis_path, 'predictions.pt'))
     
-    predictions = torch.load(os.path.join(analysis_path, 'predictions.pt'))
+#     try:
+#         for lang, raw_logits in predictions.items():
+#             to_flat_label = lambda probs : torch.argmax(probs.reshape(-1, probs.shape[-1]), dim = -1)
+#             raw_outputs = torch.argmax(raw_logits, dim = -1)
+#             flat_logits = to_flat_label(raw_outputs)
+#             outputs, labels = clean_padded_labels_pair(model, lang, flat_logits)
     
-    try:
-        for lang, raw_logits in predictions.items():
-            to_flat_label = lambda probs : torch.argmax(probs.reshape(-1, probs.shape[-1]), dim = -1)
-            raw_outputs = torch.argmax(raw_logits, dim = -1)
-            outputs, labels = clean_padded_labels_pair(model, lang, raw_outputs)
-    
-            plt.hist(outputs.numpy().flat, alpha = 0.5, color = 'r', label = 'predicted')
-            plt.hist(labels.numpy().flat, alpha = 0.5, color = 'g', label = 'true')
+#             plt.hist(outputs.numpy().flat, alpha = 0.5, color = 'r', label = 'predicted')
+#             plt.hist(labels.numpy().flat, alpha = 0.5, color = 'g', label = 'true')
             
-            accuracy = (torch.sum(outputs == labels) / outputs.shape[0]).item()
-            plt.legend()
-            plt.title(f'Frequency of class predictions vs labels. Accuracy: {round(accuracy * 100, 4)}%')
-            plt.xlabel('Numerical label')
-            plt.ylabel('Counts')
-            plt.xticks(range(len(constant.UD_POS_LABELS)), constant.UD_POS_LABELS, rotation = 45)
-            figure_path = os.path.join(analysis_path, f'prediction_comparison_val_{lang}.png')
-            plt.savefig(figure_path)
-            print(f'Figure written to: {figure_path}')
-            plt.figure()
-    except: import pdb; pdb.set_trace()
+#             accuracy = (torch.sum(outputs == labels) / outputs.shape[0]).item()
+#             plt.legend()
+#             plt.title(f'Frequency of class predictions vs labels. Accuracy: {round(accuracy * 100, 4)}%')
+#             plt.xlabel('Numerical label')
+#             plt.ylabel('Counts')
+#             plt.xticks(range(len(constant.UD_POS_LABELS)), constant.UD_POS_LABELS, rotation = 45)
+#             figure_path = os.path.join(analysis_path, f'prediction_comparison_val_{lang}.png')
+#             plt.savefig(figure_path)
+#             print(f'Figure written to: {figure_path}')
+#             plt.figure()
+#     except: import pdb; pdb.set_trace()
 
-def compare_english_prior(model):
+def get_log_q_given_input(predictions_dict, lang):
+    log_q_given_input = torch.cat([
+        outputs[1].sum(dim=1)
+        for outputs in predictions_dict[lang]
+    ], axis = 0)
+    return log_q_given_input
+    
+def compare_english_prior(model, checkpoint_path):
     
     analysis_folder = get_analysis_path(checkpoint_path)
+    prediction_dict = torch.load(os.path.join(analysis_folder, 'predictions.pt'))
     try:
         lang = 'Dutch'
         english_prior = model.get_smoothed_english_prior()
-        raw_outputs = torch.load(os.path.join(analysis_folder, 'predictions.pt'))[lang]
-        predictions, labels = clean_padded_labels_pair(model, lang, raw_outputs)
+        raw_outputs = get_log_q_given_input(prediction_dict, lang)
+        predictions, _ = clean_padded_labels_pair(model, lang, raw_outputs)
         repeated_prior = english_prior.unsqueeze(0).repeat(predictions.shape[0], 1)
         
         mean = torch.abs(predictions - repeated_prior).mean(dim = 0)
@@ -125,15 +140,17 @@ if __name__ == '__main__':
     
     checkpoint_path = os.path.join(model_folder, 'ckpts', checkpoint_name)
     model = model_type.load_from_checkpoint(checkpoint_path)
-    predictions = get_all_predictions(model, ['Dutch'])
     
     analysis_path = get_analysis_path(checkpoint_path)
-    torch.save(predictions, os.path.join(analysis_path, 'predictions.pt'))
+    predictions_path = os.path.join(analysis_path, 'predictions.pt')
+    if not os.path.exists(predictions_path):
+        raw_predictions = get_all_predictions(model, ['Dutch'])
+        analysis_path = get_analysis_path(checkpoint_path)
+        torch.save(raw_predictions, predictions_path)
+    else:
+        raw_predictions = torch.load(predictions_path)
     
     #compare_validation_predictions(model, checkpoint_path)
-    #results = compare_english_prior(model)
-    
-    print(predictions)
-    
+    results = compare_english_prior(model, checkpoint_path)
     
     

src/model/vae.py
@@ -11,12 +11,14 @@ import os
 import torch
 import torch.nn.functional as F
 from torch.distributions.uniform import Uniform
-from metric import LABEL_PAD_ID
+import math
 
+from metric import LABEL_PAD_ID
 from model import BaseVAE
 from enumeration import Split
 import constant
 import util
+
 
 class VAE(BaseVAE):
     
@@ -26,7 +28,7 @@ class VAE(BaseVAE):
         
         prior = self.get_smoothed_english_prior()
         self.prior_param = torch.nn.ParameterDict()
-        self.prior_param['prior'] = torch.nn.Parameter(prior, requires_grad = True)
+        self.prior_param['raw_prior'] = torch.nn.Parameter(prior, requires_grad = True)
         
         assert len(self.hparams.val_langs) == 1, "Validation prior code currently only designed for one validation language."
         self.validation_prior = util.apply_gpu(self.get_smoothed_prior(self.hparams.val_langs[0], Split.dev))
@@ -59,14 +61,19 @@ class VAE(BaseVAE):
         log_pi_t = self.set_padded_to_zero(batch, raw_log_pi_t)
         return log_pi_t
         
-    def calculate_KL_against_prior(self, log_q_given_input, prior):
+    def calculate_KL_against_prior(self, log_q_given_input, raw_prior):
         
-        repeated_prior = prior.unsqueeze(0).repeat(log_q_given_input.shape[0], 1)
+        prior = raw_prior.softmax(dim=-1)
+        repeated_prior = prior.reshape(1, 1, prior.shape[0]).repeat(log_q_given_input.shape[0], log_q_given_input.shape[1], 1)
         pre_sum = torch.exp(log_q_given_input) * (log_q_given_input - torch.log(repeated_prior))
         assert pre_sum.shape == log_q_given_input.shape, f'pre_sum: {pre_sum.shape}, q_given_input: {log_q_given_input.shape}'
         
         kl_divergence = torch.sum(pre_sum, axis = -1)
-        return kl_divergence.mean()
+        
+        if not torch.all(kl_divergence >= 0):
+            import pdb; pdb.set_trace()
+            
+        return kl_divergence
         
         
     def forward(self, batch):
@@ -97,14 +104,12 @@ class VAE(BaseVAE):
                     ignore_index=LABEL_PAD_ID,
             )
             
-        log_q_given_input = log_pi_t.sum(dim=1) 
-        
-        loss['KL'] = self.calculate_KL_against_prior(log_q_given_input, self.prior_param.prior)
+        loss['KL'] = self.calculate_KL_against_prior(log_pi_t, self.prior_param.raw_prior).mean()
         with torch.no_grad():
-            loss['target_KL'] = self.calculate_KL_against_prior(log_q_given_input, self.validation_prior)
+            loss['target_KL'] = self.calculate_KL_against_prior(log_pi_t, self.validation_prior).mean()
             
         loss['decoder_loss'] = loss['MSE'] + self.hparams.kl_weight * loss['KL']
-        import math
+        
         if math.isnan(loss['decoder_loss']): import pdb; pdb.set_trace()
         return loss, log_pi_t
         