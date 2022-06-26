cd5a8aa05ec101f16a19808f3ea9dc158428c64e
Nicole H Wong <wongn@node0038.inband>
2022-06-26 14:59:00 -0400

    with searched lr requested runs

---
+++ src/scripts/requested_baselines/phase_3_variants/normal.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Taken from Shijie Wu's crosslingual-nlp repository.
+# See LICENSE in this codebase for license information.
+
+# Changes made relative to original:
+# Changed to python3, directory where this is run
+# Simplified `example/surprising-mbert/evaluate.sh` script due to no need for non-POS evaluation by removing irrelevant code, variables, options, etc.
+# Added hyperparameters that yield best performance on English val for last layer finetuned BERT.
+# Edited arguments.
+# Edited to run the train_decoder script instead.
+
+seed=${1:-42}
+model=${2:-"bert-base-multilingual-cased"}
+task=${3:-"udpos"}
+
+model_name=$(echo "$model" | tr '/' '\n' | tail -n1)
+
+save_path=${4:-"./experiments/phase_3_variants/normal"}
+
+src="English Dutch"
+tgt="English Dutch"
+data_path=${5:-"../ud-treebanks-v1.4"}
+decoder_checkpoint=${6:-"./experiments/normal/phase_2/linear_lr_1e-1_bs16/version_0/ckpts/ckpts_epoch=9-val_English_decoder_loss=54.691.ckpt"}
+
+bs=16
+ep=10
+lr=1e-2
+
+python3 src/train_decoder.py \
+    --seed "$seed" \
+    --task "$task" \
+    --data_dir "$data_path" \
+    --trn_langs $src \
+    --val_langs $tgt \
+    --pretrain "$model" \
+    --batch_size $bs \
+    --learning_rate $lr \
+    --max_epochs $ep \
+    --warmup_portion 0.1 \
+    --default_save_path "$save_path" \
+    --exp_name linear_lr_"$lr"_bs"$bs" \
+    --gpus 1 \
+    --decoder_checkpoint "$decoder_checkpoint" \
+    --schedule "reduceOnPlateau" \
+    --prior_type "optimized_data"
+    
---
+++ src/scripts/requested_baselines/phase_3_variants/random.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Taken from Shijie Wu's crosslingual-nlp repository.
+# See LICENSE in this codebase for license information.
+
+# Changes made relative to original:
+# Changed to python3, directory where this is run
+# Simplified `example/surprising-mbert/evaluate.sh` script due to no need for non-POS evaluation by removing irrelevant code, variables, options, etc.
+# Added hyperparameters that yield best performance on English val for last layer finetuned BERT.
+# Edited arguments.
+# Edited to run the train_decoder script instead.
+
+seed=${1:-42}
+model=${2:-"bert-base-multilingual-cased"}
+task=${3:-"udpos"}
+
+model_name=$(echo "$model" | tr '/' '\n' | tail -n1)
+
+save_path=${4:-"./experiments/phase_3_variants/random"}
+
+src="English Dutch"
+tgt="English Dutch"
+
+data_path=${5:-"../ud-treebanks-v1.4"}
+encoder_checkpoint=${6:-"./experiments/normal/phase_1/version_0/ckpts/ckpts_epoch=2-val_English_acc=96.807.ckpt"}
+
+bs=16
+ep=20
+lr=1e-1
+
+python3 src/train_decoder.py \
+    --seed "$seed" \
+    --task "$task" \
+    --data_dir "$data_path" \
+    --trn_langs $src \
+    --val_langs $tgt \
+    --pretrain "$model" \
+    --batch_size $bs \
+    --learning_rate $lr \
+    --max_epochs $ep \
+    --warmup_portion 0.1 \
+    --default_save_path "$save_path" \
+    --encoder_checkpoint $encoder_checkpoint \
+    --exp_name linear_lr_"$lr"_bs"$bs" \
+    --gpus 1
