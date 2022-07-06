changes for correct softmax, vae, etc

src/model/base_vae.py
@@ -64,16 +64,17 @@ class BaseVAE(Tagger):
             else:
                 self.auxiliary_mu = torch.nn.Linear(self.hidden_size, self.hparams.auxiliary_size)
 
-        self._selection_criterion = f'val_{self.hparams.trn_langs[0]}_decoder_loss_epoch_monitor'
-        self._comparison_mode = 'min'
         self.optimization_loss = 'decoder_loss'
+        self._selection_criterion = f'val_{self.hparams.trn_langs[0]}_{self.optimization_loss}_epoch_monitor'
+        self._comparison_mode = 'min'
         self.metric_names = [
             'MSE',
             'decoder_loss',
         ]
         if self.use_auxiliary:
             self.metric_names.append('auxiliary_KL')
-    
+        self.setup_metrics()
+        
     # Below forward-related methods:
     # Shijie Wu's code, but with decoder logic added and irrelevant options removed,
     # and variables renamed for notation consistency.
@@ -95,10 +96,6 @@ class BaseVAE(Tagger):
         )
         return clean_tensor
         
-    def calculate_decoder(self, pi_t):
-        mu_t = self.decoder(F.softmax(pi_t, dim=-1))
-        return mu_t
-        
     def calculate_decoder_loss(self, batch, hs, pi_t):
         loss = {}
         if self.use_auxiliary:
@@ -118,7 +115,7 @@ class BaseVAE(Tagger):
         else:
             decoder_input = pi_t
 
-        mu_t = self.calculate_decoder(decoder_input)
+        mu_t = self.decoder(decoder_input)
 
         loss['MSE'] = self.masked_mse_loss(batch, mu_t, hs)
         auxiliary_loss = (self.hparams.auxiliary_kl_weight * loss['auxiliary_KL']) if self.use_auxiliary else 0
@@ -159,15 +156,8 @@ class BaseVAE(Tagger):
 
         self.add_language_to_batch_output(loss, batch)
         return loss, None
-        
     # end forward-related methods
     
-    # Renamed variables, function, direct return of loss_dict, no self.log for loss
-    # Updated assert message and metrics indexing
-    def step_helper(self, batch, prefix):
-        loss_dict, _ = self.__call__(batch)
-        return loss_dict
-        
     @classmethod
     def add_model_specific_args(cls, parser):
         parser = Tagger.add_model_specific_args(parser)

src/model/vae.py
@@ -122,6 +122,12 @@ class VAE(BaseVAE):
             ignore_index=LABEL_PAD_ID,
         )
         return encoder_loss
+        
+    def calculate_one_hot_from_probabilities(self, pi_tilde_t):
+        predicted_labels = pi_tilde_t.argmax(dim=-1)
+        one_hot_predictions = F.one_hot(predicted_labels, num_classes = self.nb_labels)
+        assert pi_tilde_t.shape == one_hot_predictions.shape, f'pi_tilde_t: {pi_tilde_t.shape}, one_hot: {one_hot_predictions.shape}'
+        return one_hot_predictions
     
     def __call__(self, batch):
         self.model.eval()
@@ -132,16 +138,23 @@ class VAE(BaseVAE):
         log_pi_t = self.calculate_log_pi_t(batch, hs)
         
         # Calculate predicted mean
-        if self.hparams.pos_kl_weight > 0:
+        assert self.classifier.training == self.decoder.training
+        is_train = self.classifier.training
+        if self.hparams.pos_kl_weight > 0 and is_train:
             uniform_sample = Uniform(torch.zeros(log_pi_t.shape), torch.ones(log_pi_t.shape)).rsample()
             noise = util.apply_gpu(-torch.log(-torch.log(uniform_sample)))
         
             unnormalized_pi_tilde_t = (log_pi_t + noise) / self.hparams.temperature
             raw_pi_tilde_t = F.softmax(unnormalized_pi_tilde_t, dim=-1)
-            pi_tilde_t = self.set_padded_to_zero(batch, raw_pi_tilde_t)
         else:
-            pi_tilde_t = log_pi_t
+            raw_pi_tilde_t = torch.exp(log_pi_t)
+        
+        # For eval, do NOT add noise, but DO convert to one-hot representation
+        if not is_train:
+            # Need float for compatibility in cleaning later (as model input)
+            raw_pi_tilde_t = self.calculate_one_hot_from_probabilities(raw_pi_tilde_t).float()
             
+        pi_tilde_t = self.set_padded_to_zero(batch, raw_pi_tilde_t)
         loss = self.calculate_decoder_loss(batch, hs, pi_tilde_t)
     
         # Only softmax if the prior is optimized (otherwise it's already probability)