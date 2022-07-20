e9419c5bc74718f51aa978bb18ad5461777afb93
Nicole H Wong <wongn@node0005.inband>
2022-07-20 11:07:51 -0400

    plotting code for weight sweeps, old structure

--- src/plot_sweeps_old_structure.py
+++ src/plot_sweeps_old_structure.py
@@ -1,4 +1,3 @@
-
 import plotly.express as px
 import glob
 
@@ -32,41 +31,35 @@ def get_heatmap(df, row_key, column_key, value_key, analysis_path):
         fig.write_html(os.path.join(analysis_path, f'{modifier}{value_key}.html'))
     return raw_scores
     
-def find_hparam(hparam_name, hparam_list):
-    hparam_prefix = f'--{hparam_name}='
-    matches = list(filter(lambda s : hparam_prefix in s, hparam_list))
-    assert len(matches) == 1, f'{hparam_name}, {hparam_list}, {matches}'
-    return matches[0].replace(hparam_prefix, "")
-    
-    
 if __name__ == '__main__':
     
+    sweep_id, sweep_name = 'vatgk860', 'no_sample_latent_weights'
+    analysis_path = f'./experiments/sweeps/{sweep_name}'
     weights = [1, 0.1, 0.01, 0.001, 1e-4, 1e-5, 1e-6, 0][::-1]
     
     # Compose the dataframe first
-    sweep_id, sweep_name = 'tmbtjd5d', 'latent_weights'
-    analysis_path = f'./experiments/sweeps/{sweep_name}'
     extension = 'yaml'
     clean_extension = lambda s : s.split('/')[-1].replace(f'.{extension}', '')
     
     records = []
-    config_paths = list(glob.glob(f'./experiments/sweeps/{sweep_name}/*/wandb/run-*/files'))
-    for config_folder in config_paths:
-        with open(os.path.join(config_folder, 'wandb-metadata.json'), 'r') as f:
-            config = json.load(f)["args"]
-            kl, mse = float(find_hparam('latent_kl_weight', config)), float(find_hparam('mse_weight', config))
-        with open(os.path.join(config_folder, 'wandb-summary.json'), 'r') as f:
+    config_paths = list(glob.glob(f'./wandb/sweep-{sweep_id}/*'))
+    for config_path in config_paths:
+        run_id = clean_extension(config_path).split('-')[-1]
+        with open(config_path, 'r') as f:
+            config = yaml.safe_load(f)
+            kl, mse = config['latent_kl_weight']['value'], config['mse_weight']['value']
+        run_paths = list(filter(lambda s: run_id in s, glob.glob(f'./wandb/*')))
+        assert len(run_paths) == 1, f'{run_id}, {run_paths}'
+        with open(os.path.join(run_paths[0], 'files', 'wandb-summary.json'), 'r') as f:
             results = json.load(f)
-            for_df_results = {'latent_kl_weight' : kl, 'mse_weight' : mse }
+            for_df_results = { 'run_id' : run_id, 'latent_kl_weight' : kl, 'mse_weight' : mse }
             for phase in ['train', 'val']:
                 for lang in ['English', 'Dutch']:
                     key = f'best_{phase}_{lang}_acc_epoch'
                     for_df_results[key] = results[key]
         records.append(for_df_results)
-    import pdb; pdb.set_trace()
     df = pd.DataFrame.from_records(records)
     for phase in ['train', 'val']:
         for lang in ['English', 'Dutch']:
             key = f'best_{phase}_{lang}_acc_epoch'
             scores = get_heatmap(df, 'latent_kl_weight', 'mse_weight', key, analysis_path)
-                
